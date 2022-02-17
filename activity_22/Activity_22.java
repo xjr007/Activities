ee2a3c03f7c8bdaea45d51183461da91d98f5a3a
Aaqiel <abaaqielbehardien@gmail.com>
2022-02-17 14:08:49 +0200

    Added Activity 22

---
+++ activity_22/Activity_22.java
@@ -0,0 +1,19 @@
+/*		Question
+ *
+ *		Create your own functional interface
+ *		and functional interface method.
+ *
+ *		Create a lambda and run code that uses
+ *		the functional interface method.
+ *
+ *		Print your results to console.
+ *		
+ *		Restrictions
+ *		------------
+ *
+ *		1. ONLY CREATE 1 FUNCTIONAL INTERFACE
+ *		2. YOUR LAMBDA MAY TAKE ANY TYPE OF ARGUMENT
+ *		3. YOUR LAMBDA MAY ONLY TAKE A MAX OF 2 ARGUMENTS
+ *
+ *
+ * */
