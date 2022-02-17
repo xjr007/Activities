dd28a432606efbe2386f2bb23a0e6e5dcda6855f
Aaqiel <abaaqielbehardien@gmail.com>
2022-02-17 14:46:34 +0200

    Added Activity 24

---
+++ activity_24/Activity_24.java
@@ -0,0 +1,12 @@
+/*
+ *	Question
+ *
+ *	1. Create your own functional interface.
+ *	2. Add an additional 3 methods to the interface.
+ *	3. Create a lambda and run it.
+ *	4. Run the other 3 methods you created in the
+ *	   interface.
+ *	5. Print results to console.
+ *
+ *
+ * */
