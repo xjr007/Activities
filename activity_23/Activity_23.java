982e736447b790c999a29c2f11d2b8e5321fa227
Aaqiel <abaaqielbehardien@gmail.com>
2022-02-17 14:17:58 +0200

    Added Activity 23

---
+++ activity_23/Activity_23.java
@@ -0,0 +1,25 @@
+/*		
+ *
+ *		NB!
+ *		___________________________________________
+ *
+ *		Requirements to do the following activity
+ *		
+ *		1. Copy the Java code file from Activity 22
+ *		   into the current directory
+ *		
+ *		___________________________________________
+ *
+ *		Question
+ *
+ *		1.	Override the functional interface method 
+ *			you created in activity 22.
+ *		2. 	Execute the newly created method.
+ *		3.	You may run any code inside the method.
+ *		4. 	Print your results to console.
+ * */
+
+class A {
+// Do code here
+
+}
