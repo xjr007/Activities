ac879b522ab540fdbb65523e34b2357862c9a443
Mubashir <mubashirisaacs.01@icloud.com>
2022-02-25 14:48:52 +0200

    Protected practice

--- lessons/InheritingMembers/protected/app/src/com/packageA/A.java
+++ lessons/InheritingMembers/protected/app/src/com/packageA/A.java
@@ -2,4 +2,27 @@ package com.packageA;
 
 public class A {
 	// create protected members
+
+	protected int x = 20;
+	protected String name = "Mubashir";
+	protected String surname = "Isaacs";
+	protected static String hobby = "Camping";
+	 
+
+	protected static void methA() {
+		A o = new A();
+		System.out.println(o.name + " " + hobby + o.x);
+
+	}
+
+	protected String methB(String s) {
+		return surname;
+
+	}
+
+	protected int methC(int y) {
+		int z = 24;
+		return z;
+
+	}
 }
