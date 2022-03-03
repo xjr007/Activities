5f1461fbe1f932f729267649fd2f42c1cef77d7b
Aaqiel <abaaqielbehardien@gmail.com>
2022-03-03 14:06:01 +0200

    Polymorphism examples

--- lessons/polymorphism/examples/A.java
+++ lessons/polymorphism/examples/A.java
@@ -14,20 +14,42 @@ class B extends A {
 	public int getHeight() {
 		return 17;
 	}
+
+	public String getSurname() {
+		return "Richard";
+	}
+
 }
 
 class C extends B {
+
+	public int getNumber() {
+		return 8426;
+	}
 	public static void main(String[] args) {
 		C o = new C();
+
+		System.out.println(o.getSurname());
+
 		A ao = o;
 		// Object o is dereferenced to A
 		System.out.println(ao.getName());
 		System.out.println(ao.getHeight());
+		
+		//System.out.println(ao.getNumber());
+		//System.out.println(ao.getSurname());
 
 		B bo = o;
 		// Object o is dereferenced to B
 		System.out.println(bo.getName());
 		System.out.println(bo.getHeight());
+
+		B b = new B();
+
+		A ba = b;
+
+		A a = new B();
+		B ab = (B) a;
 
 	}
 }
